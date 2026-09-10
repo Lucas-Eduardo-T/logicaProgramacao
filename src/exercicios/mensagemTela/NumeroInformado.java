c54c3d220aaa3023702655d4d2958f6013151448
Lucas <contatolucaster@gmail.com>
2026-09-09 22:45:11 -0300

    Exercício 2
    é o exercício número 2 da lista

---
+++ src/exercicios/mensagemTela/NumeroInformado.java
@@ -0,0 +1,16 @@
+package exercicios.mensagemTela;
+
+import java.util.Scanner;
+
+// Faça um Programa que peça um número inteiro e então mostre a mensagem:
+//"O número informado foi [número]."
+public class NumeroInformado {
+    static void main() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite um número inteiro para que ele apareça" +
+                " na tela:");
+        int numero = entrada.nextInt();
+        System.out.println("O número informado foi: " + numero + ".");
+        entrada.close();
+    }
+}
