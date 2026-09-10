2692dc5e747250eb086921b7975a4d5c23bd7b7b
Lucas <contatolucaster@gmail.com>
2026-09-09 22:54:22 -0300

    exercício 3
    O exercício 3 da lista.

---
+++ src/exercicios/mensagemTela/SomaDoisNumeros.java
@@ -0,0 +1,16 @@
+package exercicios.mensagemTela;
+
+import java.util.Scanner;
+// Faça um Programa que peça dois números inteiros e imprima a soma.
+public class SomaDoisNumeros {
+    static void main() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o primeiro número inteiro: ");
+        int numero1 = entrada.nextInt();
+        System.out.println("Digite o segundo número inteiro: ");
+        int numero2 = entrada.nextInt();
+        int soma = numero1 + numero2;
+        System.out.println("A soma entre os valores " + numero1 + " e " + numero2 + " é: " +
+        soma + ".");
+    }
+}
